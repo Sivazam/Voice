d9716fd431785c8ee14479d2222ea74728967ff4
sivazam <tejarongala@gmail.com>
2025-12-31 04:51:21 +0530

    logo slider img fix

--- public/logos/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig } */
-const nextConfig = {
-  // Remove deprecated experimental.appDir as it's default in Next.js 15
-  // Remove TypeScript compiler options as they belong in tsconfig.json
-};
-
-export default nextConfig;
