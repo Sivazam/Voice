ce78efb1968a3ad39cf3e95af5f53be93b5e18f8
sivazam <tejarongala@gmail.com>
2026-01-07 19:51:59 +0530

    digi card fix 7

--- src/app/digital-card/setup/page.tsx
+++ src/app/digital-card/setup/page.tsx
@@ -26,7 +26,7 @@ function AuthModal({
     onSuccess
 }: {
     onSuccess: () => void;
-}): JSX.Element {
+) {
     const { auth: storeAuth, setPhoneNumber, loginSuccess } = useDigitalCardStore();
     const [phone, setPhone] = useState('');
     const [otp, setOtp] = useState('');
